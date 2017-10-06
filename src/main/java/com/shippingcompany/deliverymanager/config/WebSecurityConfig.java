e69825d65ea8bdf818601eb082f4a724722a7331
VitorGCarrilho <vitorg.carrilho@gmail.com>
2017-10-06 08:38:35 -0300

    Swagger adjusts

--- src/main/java/com/shippingcompany/deliverymanager/config/WebSecurityConfig.java
+++ src/main/java/com/shippingcompany/deliverymanager/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http	
 			.authorizeRequests()
 					.antMatchers("/api/v1/**").hasAuthority(Authority.API_USER.toString())
-					.antMatchers("/swagger-ui.html").permitAll()
+					.antMatchers("/swagger**").permitAll()
 					.anyRequest().authenticated()
 					.and().csrf().disable()
 			.httpBasic();
